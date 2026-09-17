01d4fcb0564a64e865e8d5f3e2234c150842a87b
Andrew N. <kot2usinsk@gmail.com>
2026-09-17 15:35:33 +0300

    все сделал

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,7 @@
 #include "pch.h"
 
-#include "h.h" // Предполагается, что ваш класс находится в этом файле
+#include "h.h"
 
-// Тест на корректное создание объекта и вычисление площади
 TEST(TriangleTest, ValidConstructorAndArea) {
     Triangle t(5.0, 4.0);
     EXPECT_DOUBLE_EQ(t.getS(), 5.0);
@@ -10,43 +9,24 @@ TEST(TriangleTest, ValidConstructorAndArea) {
     EXPECT_DOUBLE_EQ(t.area(), 10.0);
 }
 
-// Тест сеттеров с корректными значениями
-TEST(Trian  gleTest, ValidSetters) {
-    Triangle t(1.0, 1.0);
 
-    t.setS(10.0);
-    EXPECT_DOUBLE_EQ(t.getS(), 10.0);
-
-    t.setH(3.0);
-    EXPECT_DOUBLE_EQ(t.getH(), 3.0);
-
-    EXPECT_DOUBLE_EQ(t.area(), 15.0);
-}
-
-// Тест на выброс исключения при некорректных значениях в конструкторе
 TEST(TriangleTest, InvalidConstructorArgs) {
-    // Основание <= 0
     EXPECT_THROW(Triangle t(0, 5.0), std::invalid_argument);
     EXPECT_THROW(Triangle t(-3.0, 5.0), std::invalid_argument);
 
-    // Высота <= 0
     EXPECT_THROW(Triangle t(5.0, 0), std::invalid_argument);
     EXPECT_THROW(Triangle t(5.0, -1.0), std::invalid_argument);
 }
 
-// Тест на выброс исключения в сеттерах
 TEST(TriangleTest, InvalidSetterArgs) {
     Triangle t(5.0, 4.0);
 
-    // Проверка setS
     EXPECT_THROW(t.setS(0), std::invalid_argument);
     EXPECT_THROW(t.setS(-2.5), std::invalid_argument);
 
-    // Проверка setH
     EXPECT_THROW(t.setH(0), std::invalid_argument);
     EXPECT_THROW(t.setH(-10.0), std::invalid_argument);
 
-    // Значения не должны измениться после неудачных попыток
     EXPECT_DOUBLE_EQ(t.getS(), 5.0);
     EXPECT_DOUBLE_EQ(t.getH(), 4.0);
 }
